ae00398dd2179a48eb0663ac054d9906ef4c39a6
Harshit Jena <jenaharshit@gmail.com>
2026-03-24 15:11:29 +0530

    Add files via upload

---
+++ to print gl bajaj using dot pattern.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main(){
+    //G
+    printf(" ***  \n");
+    printf("*   * \n");
+    printf("*     \n");
+    printf("*  ** \n");
+    printf("*   * \n");
+    printf(" ***  \n");
+
+    //L
+    printf("*     \n");
+    printf("*     \n");
+    printf("*     \n");
+    printf("*     \n");
+    printf("*     \n");
+    printf("***   \n");
+
+    printf("   ");
+
+    //B
+    printf("****   \n");
+    printf("*  *   \n");
+    printf("*  *   \n");
+    printf("****   \n");
+    printf("*  *   \n");
+    printf("****   \n");
+
+    //A
+    printf(" ***   \n");
+    printf("*   *  \n");
+    printf("*   *  \n");
+    printf("*****  \n");
+    printf("*   *  \n");
+    printf("*   *  \n");
+
+
+    //J
+    printf("*****  \n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf(" **    \n");
+
+    //A
+    printf(" ***   \n");
+    printf("*   *  \n");
+    printf("*   *  \n");
+    printf("*****  \n");
+    printf("*   *  \n");
+    printf("*   *  \n");
+
+    //J
+    printf("*****  \n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf(" **    \n");
+
+    return 0;
+
+}
